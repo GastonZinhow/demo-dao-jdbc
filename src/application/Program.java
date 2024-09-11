1dc8c959c7e78193609f637d91459f0581ed04ff
Matheus Gaston <theteusviana@gmail.com>
2024-09-11 10:39:03 -0300

    feat: findById and reusing instatiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Date;
+import java.sql.Connection;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,13 +12,11 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println("=== TEST 1: seller findById ===");
 		
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 	}
 
